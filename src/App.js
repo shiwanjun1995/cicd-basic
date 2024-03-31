2d6b0ada8d493fa1629c9c6fc1d39722efcd8e6a
wanjun.shi <wanjun.shi@transsion.com>
2024-03-31 16:20:01 +0800

    chore: yaml配置更新为最新版本

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // useState 钩子来管理当前时间 currentTime 的状态
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
+  // useEffect 钩子来启动一个每隔一秒更新一次状态的定时器
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +27,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React！
+          Learn React
+          <br/>
+          <strong>{currentTime}</strong>
         </a>
       </header>
     </div>

c50267668cb8b3d6f37ed1b1480ed21631def409
wanjun.shi <wanjun.shi@transsion.com>
2024-03-31 17:18:37 +0800

    fix: update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         >
           Learn React
           <br/>
-          <strong>Right now: {currentTime}</strong>
+          <strong style={{color: "red"}}>Right now: {currentTime}</strong>
         </a>
       </header>
     </div>
